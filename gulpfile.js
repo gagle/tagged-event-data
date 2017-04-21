7caf7bd6a37dce357987a47208cf10867b223dd1
Gabriel Llamas <gabriel.llamas@erni-espana.es>
2017-04-21 15:52:53 +0200

    removed old transpilation into src and test

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,7 @@ const del = require('del');
 
 gulp.task('clean', () => del([
   'lib',
-  'es',
-  'src/**/*.js',
-  'src/**/*.js.map',
-  'src/**/*.d.ts',
-  'test/**/*.js',
-  'test/**/*.js.map*',
-  'test/**/*.d.ts'
+  'es'
 ]));
 
 gulp.task('build:es', () =>
@@ -36,31 +30,7 @@ gulp.task('build:lib', () =>
   .pipe(gulp.dest('lib'))
 );
 
-gulp.task('build:src', () =>
-  gulp.src('src/**/*.ts')
-  .pipe(sourcemaps.init())
-  .pipe(tsc.createProject('tsconfig.json')())
-  .on('error', err => {
-    process.exit(1);
-  })
-  .js.pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('src'))
-);
-
-gulp.task('build:test', () =>
-  gulp.src('test/**/*.ts')
-  .pipe(sourcemaps.init())
-  .pipe(tsc.createProject('tsconfig.json')())
-  .on('error', err => {
-    process.exit(1);
-  })
-  .js.pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('test'))
-);
-
 gulp.task('build', gulp.series(
   'build:es',
-  'build:lib',
-  'build:src',
-  'build:test'
+  'build:lib'
 ));
