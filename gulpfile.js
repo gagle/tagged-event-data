64d753bfa0c5706ec54720f720fd58b4c708c522
Gabriel Llamas <gabriel.llamas@erni-espana.es>
2017-04-26 09:47:16 +0200

    truly test on supported node versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
 const tsc = require('gulp-typescript');
 const del = require('del');
 
 gulp.task('clean', () => del([
   'lib',
   'es',
-  'npm-debug.log*'
+  'npm-debug.log*',
+  'src/**/*.js',
+  'test/**/*.js'
 ]));
 
 gulp.task('build:es', () =>
@@ -24,14 +25,36 @@ gulp.task('build:es', () =>
 
 gulp.task('build:lib', () =>
   gulp.src('src/**/*.ts')
-  .pipe(tsc.createProject('tsconfig.json')())
+  .pipe(tsc.createProject('tsconfig.json', {
+    declaration: true
+  })())
   .on('error', err => {
     process.exit(1);
   })
   .pipe(gulp.dest('lib'))
 );
 
+gulp.task('build:src', () =>
+  gulp.src('src/**/*.ts')
+  .pipe(tsc.createProject('tsconfig.json')())
+  .on('error', err => {
+    process.exit(1);
+  })
+  .pipe(gulp.dest('src'))
+);
+
+gulp.task('build:test', () =>
+  gulp.src('test/**/*.ts')
+  .pipe(tsc.createProject('tsconfig.json')())
+  .on('error', err => {
+    process.exit(1);
+  })
+  .pipe(gulp.dest('test'))
+);
+
 gulp.task('build', gulp.series(
   'build:es',
-  'build:lib'
+  'build:lib',
+  'build:src',
+  'build:test'
 ));
